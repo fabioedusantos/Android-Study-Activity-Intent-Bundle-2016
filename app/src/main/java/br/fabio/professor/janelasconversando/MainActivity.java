3472138b916923858c8fe308de6873beb28fd408
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2016-09-14 20:07:00 -0300

    feat(navigation): adiciona CadastroActivity e navegação com Intent

--- app/src/main/java/br/fabio/professor/janelasconversando/MainActivity.java
+++ app/src/main/java/br/fabio/professor/janelasconversando/MainActivity.java
@@ -1,7 +1,9 @@
 package br.fabio.professor.janelasconversando;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void abrirNovaJanela(View v){
+        Intent i = new Intent(MainActivity.this, CadastroActivity.class);
+        startActivity(i);
     }
 }
