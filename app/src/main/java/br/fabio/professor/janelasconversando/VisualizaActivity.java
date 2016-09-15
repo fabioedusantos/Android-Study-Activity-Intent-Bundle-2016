48f0075f6bd83b8a10d9766c87ea6ebb89e8e7a2
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2016-09-15 19:34:00 -0300

    feat(navigation): cria Activity para visualização dos dados

---
+++ app/src/main/java/br/fabio/professor/janelasconversando/VisualizaActivity.java
@@ -0,0 +1,12 @@
+package br.fabio.professor.janelasconversando;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisualizaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualiza);
+    }
+}
