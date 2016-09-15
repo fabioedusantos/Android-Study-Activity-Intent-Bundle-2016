40238e2b10f692ea82a6febb7a44412086aa605b
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2016-09-14 22:26:00 -0300

    feat(form): adiciona campos de nome, endereço e email

--- app/src/main/java/br/fabio/professor/janelasconversando/CadastroActivity.java
+++ app/src/main/java/br/fabio/professor/janelasconversando/CadastroActivity.java
@@ -2,11 +2,20 @@ package br.fabio.professor.janelasconversando;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
+    private EditText nome;
+    private EditText endereco;
+    private EditText email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        nome = (EditText) findViewById(R.id.txt_nome);
+        endereco = (EditText) findViewById(R.id.txt_endereco);
+        email = (EditText) findViewById(R.id.txt_email);
     }
 }
