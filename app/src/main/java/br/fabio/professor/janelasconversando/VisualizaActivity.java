7153633c0e28788ab4b201e0307230cdddebc7e8
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2016-09-16 20:16:00 -0300

    feat(data): recupera mensagens recebidas utilizando Bundle

--- app/src/main/java/br/fabio/professor/janelasconversando/VisualizaActivity.java
+++ app/src/main/java/br/fabio/professor/janelasconversando/VisualizaActivity.java
@@ -4,9 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class VisualizaActivity extends AppCompatActivity {
+
+    private String nome;
+    private String endereco;
+    private String email;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualiza);
+
+        //pegamos todos os extras enviados da janela anterior
+        Bundle extras = getIntent().getExtras();
+        nome = extras.getString("nome");
+        endereco = extras.getString("endereco");
+        email = extras.getString("email");
     }
 }
