689ed35cf40168d230c08f21c5bf80359ce1f21d
Fábio Eduardo dos Santos <fabioedusantos@gmail.com>
2016-09-15 21:58:00 -0300

    feat(data): envia dados do formulário com Intent.putExtra

--- app/src/main/java/br/fabio/professor/janelasconversando/CadastroActivity.java
+++ app/src/main/java/br/fabio/professor/janelasconversando/CadastroActivity.java
@@ -1,7 +1,9 @@
 package br.fabio.professor.janelasconversando;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
@@ -17,5 +19,13 @@ public class CadastroActivity extends AppCompatActivity {
         nome = (EditText) findViewById(R.id.txt_nome);
         endereco = (EditText) findViewById(R.id.txt_endereco);
         email = (EditText) findViewById(R.id.txt_email);
+    }
+
+    public void salvar(View v){
+        Intent i = new Intent(CadastroActivity.this, VisualizaActivity.class);
+        i.putExtra("nome", nome.getText().toString());
+        i.putExtra("endereco", endereco.getText().toString());
+        i.putExtra("email", email.getText().toString());
+        startActivity(i);
     }
 }
